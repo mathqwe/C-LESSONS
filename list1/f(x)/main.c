3a3bdb08e5a213a20316a36c932054db49be1127
mathqwe <matheusferreirasantanaqwe@gmail.com>
2024-09-17 05:11:45 -0300

    programa de f(x)

--- list1/f(x)/main.c
+++ list1/f(x)/main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <math.h>
 
-int main () {
-    printf("Hello world");
+// este programa deve calcular f(x) = √x + (x/2) + x ^ x | x é um número real
+// função pow(x,y), tem dois argumentos e retorna x ^ y
+// função sqrt(x) retorna ²√x
+
+int main()
+{
+    float xInputed, xOutput;
+
+    printf("Esse programa calcula f(x) = √x + (x/2) + x ^ x\n");
+    printf("Digite um valor para x: ");
+
+    if (scanf("%f", &xInputed) != 1 || xInputed < 0 ) {
+        printf("\nVocê digtou um caratere inválido\n");
+        return 1;
+    }
+
+    xOutput = sqrt(xInputed) + (xInputed / 2) + pow(xInputed, xInputed);
+
+    printf("\nO valor de f(%.0f) é %.1f\n", xInputed, xOutput);
+
     return 0;
 }
